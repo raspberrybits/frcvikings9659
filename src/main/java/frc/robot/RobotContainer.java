bcad9c734407c0aa7629a57a4c947f36e191f30d
Arjun <62486112+arjunalwe@users.noreply.github.com>
2024-03-28 01:29:04 -0400

    hi eric

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,6 +21,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
@@ -48,15 +49,13 @@ public class RobotContainer {
 
     private void configureBindings() {
         // Tank Drive (single controller)
+        //FOR DUAL CONTROLLER SETUP COMMENT THIS BLOCK AND UNCOMMENT THE NEXT BLOCK
         mDrivetrain.setDefaultCommand(
                 new RunCommand(
                         () -> mDrivetrain.drive(mControls.getLeftY(), mControls.getRightY()),
                         mDrivetrain));
 
-        /* 
-         * mDrivetrain.setDefaultCommand(new iDrive(mDrivetrain, mDriver.getLeftY(), mDriver.getRightY()));
-        */
-
+        /* FOR DUAL CONTROLLER SETUP UNCOMMENT THIS BLOCK
         /*
          * Tank Drive (2 controllers)
          * mDrivetrain.setDefaultCommand(
